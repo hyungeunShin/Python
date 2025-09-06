985577d2412726e8c11710156393abb8268a2c1f
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2025-09-06 11:39:37 +0900

    2025-09-06

---
+++ level_1/chapter01/Chapter01_01.py
@@ -0,0 +1,24 @@
+# print 사용법
+# 참조 : https://www.python-course.eu/python3_formatted_output.php
+
+# 기본 출력
+print('Python Start!')
+print("Python Start!")
+print()
+print('''Python Start!''')
+print("""Python Start!""")
+
+print()
+
+# separator 옵션
+print('P', 'Y', 'T', 'H', 'O', 'N')
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='|')
+print()
+print('010', '1111', '2222', sep='-')
+print()
+print('www', 'naver', 'com', sep='.')
+
+print()
+
+# end 옵션
